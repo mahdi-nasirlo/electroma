4c54ddcf4ded6d1baf589cc14f7dc344411ef2cf
mahdi-nasirlo <m.nwordpress1223@gmail.com>
2023-01-15 22:48:30 +0330

    add requirements admin and blog post

---
+++ Modules/Blog/Database/Migrations/2023_01_15_183112_create_blog_posts_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * TODO : add user relationship to Blog Post
+     * TODO : add Category relationship to Blog Post
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('blog_author_id')->nullable()->cascadeOnDelete();
+            // $table->foreign('blog_author_id')->references('id')->on('users');
+
+            // $table->foreignId('blog_category_id')->nullable()->nullOnDelete();
+            // $table->foreign("blog_category_id")->references('id')->on('blog_categories')->cascadeOnDelete();
+
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->integer('view')->nullable()->default(0);
+            $table->integer('read_time');
+            $table->date('published_at')->nullable();
+            $table->string('seo_title', 60)->nullable();
+            $table->string('seo_description', 160)->nullable();
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+};
---
+++ Modules/Blog/Entities/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Blog\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [];
+
+    protected $table = "blog_posts";
+
+    protected static function newFactory()
+    {
+        return \Modules\Blog\Database\factories\PostFactory::new();
+    }
+}
