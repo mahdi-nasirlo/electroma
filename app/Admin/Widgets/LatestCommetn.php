c5fbf571444d289e2b87570a39976915ea6e1f48
mahdi-nasirlo <m.nwordpress1223@gmail.com>
2023-01-29 18:23:11 +0330

    add log viewer and widgets to admin panel

---
+++ app/Admin/Widgets/LatestCommetn.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Admin\Widgets;
+
+use App\Models\Comment;
+use Closure;
+use Filament\Tables;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+
+class LatestCommetn extends BaseWidget
+{
+    protected static ?int $sort = 2;
+
+    protected static ?string $heading = "اخرین دیدگاه ها ";
+
+
+    protected function getTableQuery(): Builder
+    {
+        return Comment::query()->latest();
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('content')
+                ->label("دیدگاه")
+                ->searchable(),
+            TextColumn::make('child.content')
+                ->label("پاسخ کاربران")
+                ->searchable(),
+            TextColumn::make("user.name")->label('کاربر'),
+            BooleanColumn::make("is_visible")->label('قابل مشاهده')->sortable(),
+        ];
+    }
+}
---
+++ app/Admin/Widgets/LatestRequesth.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Admin\Widgets;
+
+use Filament\Notifications\Notification;
+use Filament\Tables;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\Action;
+use Modules\Service\Entities\Service;
+
+class LatestRequesth extends BaseWidget
+{
+    protected static ?string $heading = "اخرین درخواست تعمیرکار با وضعیت باز";
+
+    protected static ?int $sort = 1;
+
+    protected function getTableQuery(): Builder
+    {
+        return Service::query()->where("status", false)->latest();
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make("name")->label("نام")->searchable(),
+            TextColumn::make("mobile")->label("شماره همراه")->searchable(),
+            TextColumn::make("items.name")->label("سرویس")->searchable(),
+            TextColumn::make("message")->label("پیام")->searchable(),
+            BooleanColumn::make("status")->label("وضعیت رسیدگی")->sortable()
+        ];
+    }
+
+    protected function getTableActions(): array
+    {
+        return [
+            Action::make("رسیدگی_شد")
+                ->action(function (Service $record) {
+                    if (!$record->status) {
+                        Notification::make()
+                            ->title('وضعیت رسیدگی درخواست به "رسیدگی شد" تغییر کرد')
+                            ->success()
+                            ->send();
+                    } else {
+                        Notification::make()
+                            ->warning()
+                            ->title("وضعیت قبلا تغییر کرده است")
+                            ->send();
+                    }
+
+                    $record->update(['status' => true]);
+                })->requiresConfirmation()
+        ];
+    }
+}
