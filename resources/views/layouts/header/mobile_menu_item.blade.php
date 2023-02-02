d8f0e494d7101e15fad448a481891828e4a433f1
mahdi-nasirlo <m.nwordpress1223@gmail.com>
2023-02-02 19:37:47 +0330

    fix mobile category link

--- resources/views/layouts/header/mobile_menu_item.blade.php
+++ resources/views/layouts/header/mobile_menu_item.blade.php
@@ -62,20 +62,22 @@
 @else
 @endif --}}
 
+
 @if ($title)
     @if ($categoreis->isEmpty())
         <li>
-            <a href="#">
+            <a href="{{ isset($link) ? $link : '#' }}">
                 {{ $title }}
             </a>
         </li>
     @else
         <li class="has-submenu">
-            <a href="#" data-submenu="{{ $id }}">{{ $title }}</a>
+            <a href="{{ isset($parentLink) ? $parentLink : '#' }}"
+                data-submenu="{{ $id }}">{{ $title }}</a>
 
             <div id="{{ $id }}" class="submenu">
                 <div class="submenu-header">
-                    <a href="#" data-submenu-close="{{ $id }}">{{ $parentName }}</a>
+                    <a href="#" data-submenu-close="{{ $id }}">{{ $parentName }} </a>
                 </div>
 
                 <label>{{ $title }}</label>
@@ -86,6 +88,8 @@
                         {{-- TODO FIX DISPLY CONDITION --}}
                         {{-- @if ($category->is_visible and $category->isVisible()) --}}
                         @include('layouts.header.mobile_menu_item', [
+                            'link' => $category->link(),
+                            'parentLink' => $category->link(),
                             'categoreis' => $category->children,
                             'title' => $category->name,
                             'parentName' => $title,
