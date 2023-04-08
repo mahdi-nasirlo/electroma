9bc1c5642b3707eff406f7a5438e52952dc5cd71
mahdi-nasirlo <m.nwordpress1223@gmail.com>
2023-04-08 07:30:43 +0430

    optimized css files

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "resources/css/tiny-slider.css",
                 "resources/js/tiny-slider.js",
                 "resources/css/font.css",
                 // "resources/css/filament.css",
